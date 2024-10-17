75f47136f4cf9b095eb6f9bf861ec6739e621c06
Kate G <161684914+k8-g@users.noreply.github.com>
2024-10-17 18:55:57 +1000

    feat: middleware for random pokemon wip

--- src/controllers/pokemonController.js
+++ src/controllers/pokemonController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { getOrCreatePokemonNumber, getPokeApiData } = require('../middleware/pokeApiMiddleware');
 
 // Create an instance of just a router, not a full server 
 const router = express.Router();
@@ -23,6 +24,22 @@ router.get("/random", async (request, response) => {
 	});
 });
 
+router.get(
+	"/randommiddleware", 
+	// Middleware goes here
+	getOrCreatePokemonNumber,
+	getPokeApiData,
+
+
+
+	// Middleware is finished by this point 
+	(request, response) => {
+	response.json({
+		result: "todo!"
+	});
+});
+
 // Export the router for the app to use 
 // since we must tell the app instance to load up routers 
 module.exports = router;
+// module.exports = {pokemonRouter};
